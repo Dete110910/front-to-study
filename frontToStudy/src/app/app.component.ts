5c894f6fff0f15c3a7d7e6375400638b2903cedf
Dete110910 <72169223+Dete110910@users.noreply.github.com>
2023-05-13 10:21:01 -0500

    Update app.component.ts

--- frontToStudy/src/app/app.component.ts
+++ frontToStudy/src/app/app.component.ts
@@ -18,14 +18,14 @@ export class AppComponent {
     this.backEndInfo = {author: " ", status: "stopped", hostname: " ", date: " "};
     this.numberInfo = {author: " ", status: "stopped", hostname: " ", date: " "}
     let headers = new HttpHeaders().set('Content-Type', 'application/json');
-    this.http.get('http://localhost:8080/api/backEndInfo', {headers: headers}).subscribe(data => {
+    this.http.get('http://192.168.0.27:30010/api/server', {headers: headers}).subscribe(data => {
       this.backEndInfo = data;
       console.log(data);
     }, error => {
       console.log("No se puedo hacer la petición al primer servidor");
     });
 
-    this.http.get('http://address2:port2/api/number', {headers: headers}).subscribe(data => {
+    this.http.get('http://192.168.0.27:30013/api/server', {headers: headers}).subscribe(data => {
       console.log(data);
       this.numberInfo = data;
     }, error => {
